5e9e23f08718a2bdc9341b4ef3d94c3878626ce6
ybadr99 <yousefbadr909@gmail.com>
2023-02-07 22:12:10 +0200

    Add fixed header on popup menu

--- src/script.js
+++ src/script.js
@@ -1,19 +1,20 @@
-const menuIcon = document.querySelector(".menu-icon");
-const navMenu = document.querySelector(".nav-menu");
-const navItem = document.querySelectorAll(".nav-item");
-const logo = document.querySelector(".logo");
+const menuIcon = document.querySelector('.menu-icon');
+const navMenu = document.querySelector('.nav-menu');
+const navItem = document.querySelectorAll('.nav-item');
+const logo = document.querySelector('.logo');
 
-menuIcon.addEventListener("click", () => {
-  menuIcon.classList.toggle("active");
-  navMenu.classList.toggle("active");
-  navItem.forEach((li) => li.classList.toggle("active"));
-  logo.classList.toggle("hide");
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  navItem.forEach((li) => li.classList.toggle('active'));
+  logo.classList.toggle('hide');
+  document.querySelector('header').classList.toggle('fixed');
 });
 
 navItem.forEach((n) => {
-  n.addEventListener("click", () => {
-    menuIcon.classList.remove("active");
-    navMenu.classList.remove("active");
-    logo.classList.remove("hide");
+  n.addEventListener('click', () => {
+    menuIcon.classList.remove('active');
+    navMenu.classList.remove('active');
+    logo.classList.remove('hide');
   });
 });
